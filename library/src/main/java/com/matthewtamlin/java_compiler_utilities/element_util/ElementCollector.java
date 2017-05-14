7be221e3f19b04d13c3b2a31185d30ac21a3210c
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-14 11:03:05 +0930

    Generified return type of getCollectedElements()

--- library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/ElementCollector.java
+++ library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/ElementCollector.java
@@ -1,20 +1,17 @@
 package com.matthewtamlin.java_compiler_utilities.element_util;
 
-import com.google.common.collect.ImmutableSet;
-
 import javax.annotation.processing.AbstractProcessor;
-import javax.lang.model.element.Element;
 
 /**
  * An annotation processor which collects elements as it processes them. The implementation determines which elements
  * are retained if any.
  */
-public abstract class ElementCollector extends AbstractProcessor {
+public abstract class ElementCollector<T> extends AbstractProcessor {
 	/**
 	 * Gets the elements which have been collected thus far. This method will not return meaningful data unless
 	 * processing is complete. This method might return an empty set but it will never return null.
 	 *
 	 * @return the elements collected by this collector, not null
 	 */
-	public abstract ImmutableSet<Element> getCollectedElements();
+	public abstract T getCollectedElements();
 }
