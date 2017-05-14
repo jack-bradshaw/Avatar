bfba993ffae5e62f9d11546f41b17997353d92b1
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-14 11:44:20 +0930

    Changed generic bounds

--- library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/ElementCollector.java
+++ library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/ElementCollector.java
@@ -1,12 +1,14 @@
 package com.matthewtamlin.java_compiler_utilities.element_util;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.lang.model.element.Element;
+import java.util.Collection;
 
 /**
  * An annotation processor which collects elements as it processes them. The implementation determines which elements
  * are retained if any.
  */
-public abstract class ElementCollector<T> extends AbstractProcessor {
+public abstract class ElementCollector<T extends Collection<? extends Element>> extends AbstractProcessor {
 	/**
 	 * Gets the elements which have been collected thus far. This method will not return meaningful data unless
 	 * processing is complete. This method might return an empty set but it will never return null.
