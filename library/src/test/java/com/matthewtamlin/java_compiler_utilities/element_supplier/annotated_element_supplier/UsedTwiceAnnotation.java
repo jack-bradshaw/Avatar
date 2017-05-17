8da393a514031fc2604ad0fb10a15e19eb95d881
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-17 20:21:53 +0930

    Renamed Tag2

--- library/src/test/java/com/matthewtamlin/java_compiler_utilities/element_supplier/annotated_element_supplier/UsedTwiceAnnotation.java
+++ library/src/test/java/com/matthewtamlin/java_compiler_utilities/element_supplier/annotated_element_supplier/UsedTwiceAnnotation.java
@@ -4,4 +4,4 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.CLASS)
-public @interface Tag2 {}
+public @interface UsedTwiceAnnotation {}
