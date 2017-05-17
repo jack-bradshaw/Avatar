0239f0659bbaba86e3b2b2e3b8af2e1ba9913159
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-17 11:41:28 +0930

    Created CollectorBasedElementSupplier.java

---
+++ library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/element_supplier/CollectorBasedElementSupplier.java
@@ -0,0 +1,31 @@
+package com.matthewtamlin.java_compiler_utilities.element_util.element_supplier;
+
+import com.matthewtamlin.java_compiler_utilities.element_util.collectors.ElementCollector;
+
+import javax.lang.model.element.Element;
+import javax.tools.JavaFileObject;
+import java.util.Collection;
+
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
+/**
+ * An element supplier which uses ElementCollectors to find elements in source fiels.
+ */
+public abstract class CollectorBasedElementSupplier<T extends Collection<? extends Element>>
+		implements ElementSupplier<T> {
+	/**
+	 * @return a new instance of the element collector type used by this class
+	 */
+	protected abstract ElementCollector<T> getNewCollector();
+	
+	@Override
+	public T getFrom(final JavaFileObject source) throws CompilerMissingException {
+		checkNotNull(source, "Argument \'source\' cannot be null.");
+		
+		final ElementCollector<T> collector = getNewCollector();
+		
+		CompilerUtil.compileUsingCollector(source, collector);
+		
+		return collector.getCollectedElements();
+	}
+}
