0a82a9eaa03b1870835676147317af192330d45c
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-17 12:23:38 +0930

    Changed class to extend CollectorBasedElementSupplier

--- library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/element_supplier/RootElementSupplier.java
+++ library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/element_supplier/RootElementSupplier.java
@@ -1,25 +1,17 @@
 package com.matthewtamlin.java_compiler_utilities.element_util.element_supplier;
 
+import com.matthewtamlin.java_compiler_utilities.element_util.collectors.ElementCollector;
 import com.matthewtamlin.java_compiler_utilities.element_util.collectors.RootElementCollector;
 
 import javax.lang.model.element.Element;
-import javax.tools.JavaFileObject;
 import java.util.Set;
-
-import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
 /**
  * Gets all root elements from a JavaFileObject.
  */
-public class RootElementSupplier implements ElementSupplier<Set<Element>> {
+public class RootElementSupplier extends CollectorBasedElementSupplier<Set<Element>> {
 	@Override
-	public Set<Element> getFrom(final JavaFileObject source) throws CompilerMissingException {
-		checkNotNull(source, "Argument \'source\' cannot be null.");
-		
-		final RootElementCollector collector = new RootElementCollector();
-		
-		CompilerUtil.compileUsingCollector(source, collector);
-		
-		return collector.getCollectedElements();
+	protected ElementCollector<Set<Element>> getNewCollector() {
+		return new RootElementCollector();
 	}
-}
+} 
