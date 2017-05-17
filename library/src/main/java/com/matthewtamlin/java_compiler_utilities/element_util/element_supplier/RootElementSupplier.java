8e137b8116ae3d30ea4ebea75babcc986f544fa7
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-17 17:31:44 +0930

    Deleted class contents and superclass declaration

--- library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/element_supplier/RootElementSupplier.java
+++ library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/element_supplier/RootElementSupplier.java
@@ -1,17 +1,8 @@
 package com.matthewtamlin.java_compiler_utilities.element_util.element_supplier;
 
-import com.matthewtamlin.java_compiler_utilities.element_util.collectors.ElementCollector;
-import com.matthewtamlin.java_compiler_utilities.element_util.collectors.RootElementCollector;
-
-import javax.lang.model.element.Element;
-import java.util.Set;
-
 /**
  * Gets all root elements from a JavaFileObject.
  */
-public class RootElementSupplier extends CollectorBasedElementSupplier<Set<Element>> {
-	@Override
-	protected ElementCollector<Set<Element>> getNewCollector() {
-		return new RootElementCollector();
-	}
+public class RootElementSupplier {
+	
 }
