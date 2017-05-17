a7d0b9f6b38a0095c02a4f05b0f2ae5bb0d00bd8
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-17 15:28:58 +0930

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/collectors/SpecificIdElementCollector.java
+++ library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/collectors/SpecificIdElementCollector.java
@@ -11,11 +11,31 @@ import java.util.Set;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+/**
+ * A collector which collects elements based on ID, where element IDs are defined using the {@link ElementId}
+ * annotation.
+ */
 public class SpecificIdElementCollector extends ElementCollector<Set<Element>> {
+	/**
+	 * The elements which have been collected during processing.
+	 */
 	private final Set<Element> collectedElements = new HashSet<>();
 	
+	/**
+	 * The ID to search for. Elements will only be collected if they have an ElementId annotation with this ID.
+	 */
 	private final String id;
 	
+	/**
+	 * Constructs a new SpecificIdElementCollector. The collector will collect all elements found during processing
+	 * which have the supplied ID (as defined by {@link ElementId} annotations in the source).
+	 *
+	 * @param id
+	 * 		the ID to search for when collecting elements, not null
+	 *
+	 * @throws IllegalArgumentException
+	 * 		if {@code id} is null
+	 */
 	public SpecificIdElementCollector(final String id) {
 		this.id = checkNotNull(id, "Argument \'id\' cannot be null.");
 	}
