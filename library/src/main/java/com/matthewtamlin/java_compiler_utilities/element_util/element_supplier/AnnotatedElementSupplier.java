24ba380aeaf904da5f266fb0322823c39be809e2
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-17 18:45:48 +0930

    Added contents

--- library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/element_supplier/AnnotatedElementSupplier.java
+++ library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_util/element_supplier/AnnotatedElementSupplier.java
@@ -12,4 +12,21 @@ import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull
 
 public class AnnotatedElementSupplier {
 	
+	private final JavaFileObject source;
+	
+	public AnnotatedElementSupplier(final JavaFileObject source) {
+		this.source = checkNotNull(source, "Argument \'source\' cannot be null.");
+	}
+	
+	public Set<Element> getElements(final Class<? extends Annotation> targetAnnotation)
+			throws CompilerMissingException {
+		
+		checkNotNull(targetAnnotation, "Argument \'targetAnnotation\' cannot be null.");
+		
+		final ElementCollector<Set<Element>> collector = new AnnotatedElementCollector(targetAnnotation);
+		
+		CompilerUtil.compileUsingCollector(source, collector);
+		
+		return collector.getCollectedElements();
+	}
 }
