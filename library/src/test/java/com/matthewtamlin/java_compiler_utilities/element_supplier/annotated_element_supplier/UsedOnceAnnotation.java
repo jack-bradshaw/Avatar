671e2150338082b91e8338c1518fc89f57d3e2e2
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-17 20:21:35 +0930

    Renamed Tag1

--- library/src/test/java/com/matthewtamlin/java_compiler_utilities/element_supplier/annotated_element_supplier/UsedOnceAnnotation.java
+++ library/src/test/java/com/matthewtamlin/java_compiler_utilities/element_supplier/annotated_element_supplier/UsedOnceAnnotation.java
@@ -4,4 +4,4 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.SOURCE)
-public @interface Tag1 {}
+public @interface UsedOnceAnnotation {}
