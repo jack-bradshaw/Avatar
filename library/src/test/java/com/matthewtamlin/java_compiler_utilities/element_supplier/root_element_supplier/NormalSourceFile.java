ce014557949a6913ec30fe970cf886683d1dacf4
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-17 21:58:14 +0930

    Created test for RootElementSupplier

---
+++ library/src/test/java/com/matthewtamlin/java_compiler_utilities/element_supplier/root_element_supplier/NormalSourceFile.java
@@ -0,0 +1,9 @@
+package com.matthewtamlin.java_compiler_utilities.element_supplier.root_element_supplier;
+
+public class NormalSourceFile {
+	public String someMethod() {
+		return "something";
+	}
+}
+
+class OtherClass {}
