82b36c636e81db72d0da1763b56e7d3f19ae9ec9
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-18 11:17:29 +0930

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_supplier/RootElementSupplier.java
+++ library/src/main/java/com/matthewtamlin/java_compiler_utilities/element_supplier/RootElementSupplier.java
@@ -13,12 +13,32 @@ import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull
  * Gets all root elements from a {@link JavaFileObject}.
  */
 public class RootElementSupplier {
+	/**
+	 * The source to get elements from.
+	 */
 	private JavaFileObject source;
 	
+	/**
+	 * Constructs a new RootElementSupplier.
+	 *
+	 * @param source
+	 * 		the JavaFileObject to get elements from, not null
+	 *
+	 * @throws IllegalArgumentException
+	 * 		if {@code source} is null
+	 */
 	public RootElementSupplier(final JavaFileObject source) {
 		this.source = checkNotNull(source, "Argument \'source\' cannot be null.");
 	}
 	
+	/**
+	 * Gets all root elements from the source. This method might return an empty set, but it will never return null.
+	 *
+	 * @return all root elements found in the source, not null
+	 *
+	 * @throws CompilerMissingException
+	 * 		if there is no Java compiler available at runtime
+	 */
 	public Set<Element> getRootElements() throws CompilerMissingException {
 		final ElementCollector<Set<Element>> collector = new RootElementCollector();
 		
